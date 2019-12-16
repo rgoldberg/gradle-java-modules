a375018b7a45e369074ae734f3d29ae78ec786cf
Ross Goldberg <ross.goldberg@gmail.com>
2019-12-16 05:08:49 -0500

    extensions: use CreateStartScriptsOptionsInternal in CreateStartScriptsTaskConfigurer
    partial gradle/gradle-java-modules#28

--- src/main/kotlin/org/gradle/java/taskconfigurer/CreateStartScriptsTaskConfigurer.kt
+++ src/main/kotlin/org/gradle/java/taskconfigurer/CreateStartScriptsTaskConfigurer.kt
@@ -16,18 +16,8 @@
 package org.gradle.java.taskconfigurer
 
 
-import java.io.File
-import java.io.IOException
-import org.gradle.api.Action
-import org.gradle.api.GradleException
 import org.gradle.api.tasks.application.CreateStartScripts
-import org.gradle.java.JigsawPlugin
 import org.gradle.java.extension.CreateStartScriptsOptionsInternal
-import org.gradle.java.jdk.JAVA
-import org.gradle.java.util.doAfterAllOtherDoFirstActions
-import org.gradle.java.util.doBeforeAllOtherDoLastActions
-import org.gradle.util.TextUtil.getUnixLineSeparator
-import org.gradle.util.TextUtil.getWindowsLineSeparator
 
 
 class CreateStartScriptsTaskConfigurer: TaskConfigurer<CreateStartScripts> {
@@ -37,59 +27,4 @@ class CreateStartScriptsTaskConfigurer: TaskConfigurer<CreateStartScripts> {
 
     override val optionsInternalClass
     get() = CreateStartScriptsOptionsInternal::class.java
-
-    override fun configureTask(createStartScripts: CreateStartScripts, jigsawPlugin: JigsawPlugin) {
-        val main = createStartScripts.mainClassName
-
-        jigsawPlugin.getModuleName(main)?.let {moduleName ->
-            val classpath by lazy {createStartScripts.classpath}
-
-            createStartScripts.doAfterAllOtherDoFirstActions(Action {
-                classpath
-
-                val args = mutableListOf<String>()
-
-                createStartScripts.defaultJvmOpts?.let {
-                    args += it
-                }
-
-                args += JAVA.OPTION_MODULE_PATH
-                args += LIB_DIR_PLACEHOLDER
-
-                args += JAVA.OPTION_MODULE
-                args += main!!
-
-                createStartScripts.defaultJvmOpts = args
-                createStartScripts.mainClassName  = ""
-                createStartScripts.classpath      = createStartScripts.project.files()
-            })
-
-            createStartScripts.doBeforeAllOtherDoLastActions(Action {
-                replaceLibDirectoryPlaceholder(createStartScripts.unixScript,    "\\\$APP_HOME/lib",  getUnixLineSeparator())
-                replaceLibDirectoryPlaceholder(createStartScripts.windowsScript, "%APP_HOME%\\\\lib", getWindowsLineSeparator())
-
-                createStartScripts.mainClassName = main
-                createStartScripts.classpath     = classpath
-            })
-        }
-    }
-
-    private fun replaceLibDirectoryPlaceholder(file: File, libDirReplacement: String, lineSeparator: String) =
-        try {
-            file.readLines().stream().map {line -> LIB_DIR_PLACEHOLDER_REGEX.replace(line, libDirReplacement)}.use {lineStream ->
-                file.bufferedWriter().use {
-                    Iterable(lineStream::iterator).joinTo(it, lineSeparator, "", lineSeparator)
-                }
-            }
-        }
-        catch (ex: IOException) {
-            throw GradleException("Couldn't replace placeholder in " + file, ex)
-        }
-
-
-    companion object {
-        private const val LIB_DIR_PLACEHOLDER = "LIB_DIR_PLACEHOLDER"
-
-        private val LIB_DIR_PLACEHOLDER_REGEX = LIB_DIR_PLACEHOLDER.toRegex()
-    }
 }
